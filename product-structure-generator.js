a3ab041044050e2af1a9505cbe2a2bd3012ae2e4
Ladányi Kristóf <ladanyi.kristof@gmail.com>
2024-12-22 13:32:04 +0100

    Create skeleton for geigery product card

--- product-structure-generator.js
+++ product-structure-generator.js
@@ -54,6 +54,82 @@ class ProductStructureGenerator {
             </div>
         `
     }
+
+    createGeigeryProductItem(data, feedData) {
+        return `
+            <div class="destovka-product-geigery-card-container">
+                <div class="destovka-product-geigery-card-title">title</div>
+                <div class="destovka-product-geigery-card-info-row">
+                    <img src="obrazek.png" style="max-width: 200px" />
+
+                    <div class="destovka-product-geigery-card-input-column">
+                        <div class="destovka-product-geigery-card-input-row">
+                            <div class="destovka-product-geigery-card-color"></div>
+                            <div class="destovka-product-geigery-card-input-container">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+                                </svg>
+                                <input type="number" class="destovka-product-geigery-card-input" placeholder="0" />
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path
+                                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                                </svg>
+                                ks
+                            </div>
+                        </div>
+                        <div class="destovka-product-geigery-card-input-row">
+                            <div class="destovka-product-geigery-card-color"></div>
+                            <div class="destovka-product-geigery-card-input-container">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+                                </svg>
+                                <input type="number" class="destovka-product-geigery-card-input" placeholder="0" />
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path
+                                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                                </svg>
+                                ks
+                            </div>
+                        </div>
+                        <div class="destovka-product-geigery-card-input-row">
+                            <div class="destovka-product-geigery-card-color"></div>
+                            <div class="destovka-product-geigery-card-input-container">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+                                </svg>
+                                <input type="number" class="destovka-product-geigery-card-input" placeholder="0" />
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path
+                                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                                </svg>
+                                ks
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="destovka-product-geigery-card-footer">
+                    <div class="destovka-product-geigery-card-unit-price">cena za kus 22 Kc</div>
+                    <div>
+                        celkem
+                        <span class="destovka-product-geigery-card-total-price">1752 Kc</span>
+                        vc DPH
+                    </div>
+                </div>
+            </div>
+        `
+    }
  
     initializeSelection(container) {
         if (!container) return;
